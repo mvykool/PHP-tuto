788bfeea1185f356d35f6f1ca573b6e75666a19c
maicol hernandez <maykolandres4@gmail.com>
2020-06-14 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -60,19 +60,6 @@ $y = 4;
 
 $favcolor = "red";
 
-switch ($favcolor) {
-	case "red":
-		echo "Your favorite color is red!";
-		break;
-	case "blue":
-		echo "Your favorite color is blue!";
-		break;
-	case "green":
-		echo "Your favorite color is green!";
-		break;
-	default:
-		echo "Your favorite color is neither red, blue, nor green!";
-	}
 
 ?>
 
