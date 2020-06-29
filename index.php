009ee4ada3eb61a389b6a771b89707050d2953f8
maicol hernandez <maykolandres4@gmail.com>
2020-06-29 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -57,6 +57,23 @@ $txt2 = "W3Schools.com";
 $x = 5;
 $y = 4;
 
+
+$favcolor = "red";
+
+switch ($favcolor) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    echo "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break;
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
+
 ?>
 
 </body>
