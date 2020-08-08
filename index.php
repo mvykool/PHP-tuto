e51656958741c76d4dec4b2f9b65981b108938b1
maicol hernandez <maykolandres4@gmail.com>
2020-08-08 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -62,10 +62,12 @@ $y = 4;
 for ($x = 0; $x <= 10; $x++) {
 		echo "The number is: $x <br>";
 	}
+
+
 	$colors = array("red", "green", "blue", "yellow");
 
 	foreach ($colors as $value) {
-	  echo "$value <br>";
+	echo "$value <br>";
 	}
 
 
