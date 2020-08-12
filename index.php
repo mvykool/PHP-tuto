c1704b30e4ec17421b10d4a87893b62b241d43a5
maicol hernandez <maykolandres4@gmail.com>
2020-08-12 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ for ($x = 0; $x <= 10; $x++) {
 	$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
 
 	foreach($age as $x => $val) {
-	  echo "$x = $val<br>";
+	echo "$x = $val<br>";
 	}
 
 ?>
