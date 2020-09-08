79f1f017d85496f94a58a9ab93eac3ce7b880b07
maicol hernandez <maykolandres4@gmail.com>
2020-09-08 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -83,14 +83,14 @@ for ($x = 0; $x < 10; $x++) {
 }
 
 function familyName($fname) {
-	echo "$fname Refsnes.<br>";
-  }
-  
-  familyName("Jani");
-  familyName("Hege");
-  familyName("Stale");
-  familyName("Kai Jim");
-  familyName("Borge");
+		echo "$fname Refsnes.<br>";
+	}
+	
+	familyName("Jani");
+	familyName("Hege");
+	familyName("Stale");
+	familyName("Kai Jim");
+	familyName("Borge");
 ?>
 
 </body>
