20473bc05e01c10ba4ff41d4e88d470aeedb7843
maicol hernandez <maykolandres4@gmail.com>
2020-09-13 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -92,6 +92,14 @@ function familyName($fname) {
 	familyName("Kai Jim");
 	familyName("Borge");
 
+	function familyName($fname, $year) {
+		echo "$fname Refsnes. Born in $year <br>";
+	  }
+	  
+	  familyName("Hege", "1975");
+	  familyName("Stale", "1978");
+	  familyName("Kai Jim", "1983");
+
 ?>
 
 </body>
