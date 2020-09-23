5b7b7d400276e51264be5778c76525f46e2c960b
maicol hernandez <maykolandres4@gmail.com>
2020-09-23 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -102,9 +102,9 @@ function familyName2($fname, $year) {
 
 	function addNumbers(int $a, int $b) {
 		return $a + $b;
-	  }
-	  echo addNumbers(5, "5 days");
-	  // since strict is NOT enabled "5 days" is changed to int(5), and it will return 10
+	}
+	echo addNumbers(5, "5 days");
+	// since strict is NOT enabled "5 days" is changed to int(5), and it will return 10
 
 ?>
 
