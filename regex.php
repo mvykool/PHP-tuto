b6a41fd31ead89211d713f84383c8434464431f2
maicol hernandez <maykolandres4@gmail.com>
2020-10-13 12:00:00 -0500

    commit of the day

--- regex.php
+++ regex.php
@@ -3,7 +3,7 @@ $cars = array("Volvo", "BMW", "Toyota");
 $arrlength = count($cars);
 
 for($x = 0; $x < $arrlength; $x++) {
-  echo $cars[$x];
-  echo "<br>";
+	echo $cars[$x];
+	echo "<br>";
 }
 ?>
