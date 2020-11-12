803c95db27968222e077522537138b84b0946b1c
maicol hernandez <maykolandres4@gmail.com>
2020-11-12 12:00:00 -0500

    commit of the day

--- regex.php
+++ regex.php
@@ -2,4 +2,9 @@
 $str = "Visit W3Schools";
 $pattern = "/w3schools/i";
 echo preg_match($pattern, $str);
+
+$str = "The rain in SPAIN falls mainly on the plains.";
+$pattern = "/ain/i";
+echo preg_match_all($pattern, $str); // Outputs 4
+
 ?>
