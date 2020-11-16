a208cb708f6266d3286c1d7dd27665bd198c7674
maicol hernandez <maykolandres4@gmail.com>
2020-11-16 12:00:00 -0500

    commit of the day

--- regex.php
+++ regex.php
@@ -6,5 +6,8 @@ echo preg_match($pattern, $str);
 $str = "The rain in SPAIN falls mainly on the plains.";
 $pattern = "/ain/i";
 echo preg_match_all($pattern, $str); // Outputs 4
+$str = "Visit Microsoft!";
+$pattern = "/microsoft/i";
+echo preg_replace($pattern, "W3Schools", $str); // Outputs "Visit W3Schools!"
 
 ?>
