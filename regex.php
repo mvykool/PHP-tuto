24d90df3cf5b4fed8ecb1a1656b847f5064457f5
maicol hernandez <maykolandres4@gmail.com>
2020-11-17 12:00:00 -0500

    commit of the day

--- regex.php
+++ regex.php
@@ -5,9 +5,9 @@ echo preg_match($pattern, $str);
 
 $str = "The rain in SPAIN falls mainly on the plains.";
 $pattern = "/ain/i";
-echo preg_match_all($pattern, $str); // Outputs 4
+echo preg_match_all($pattern, $str); 
 $str = "Visit Microsoft!";
 $pattern = "/microsoft/i";
-echo preg_replace($pattern, "W3Schools", $str); // Outputs "Visit W3Schools!"
+echo preg_replace($pattern, "W3Schools", $str); 
 
 ?>
