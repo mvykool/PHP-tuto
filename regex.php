2037191f8b4108fe80a1fab28e30b39bc50adc71
maicol hernandez <maykolandres4@gmail.com>
2020-11-26 12:00:00 -0500

    commit of the day

--- regex.php
+++ regex.php
@@ -10,4 +10,8 @@ $str = "Visit Microsoft!";
 $pattern = "/microsoft/i";
 echo preg_replace($pattern, "W3Schools", $str); 
 
+$str = "Apples and bananas.";
+$pattern = "/ba(na){2}/i";
+echo preg_match($pattern, $str); // Outputs 1
+
 ?>
