30241bbe62f64d63c3b4d0efe7bfe6c2bb82277e
maicol hernandez <maykolandres4@gmail.com>
2020-12-10 12:00:00 -0500

    commit of the day

--- advanced.php
+++ advanced.php
@@ -3,4 +3,6 @@ echo "Today is " . date("Y/m/d") . "<br>";
 echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l");
+&copy; 2010-<?php echo date("Y");?>
+
 ?>
