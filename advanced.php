9fcfff32257f52017b042996665e6d0326738140
maicol hernandez <maykolandres4@gmail.com>
2020-12-22 12:00:00 -0500

    commit of the day

--- advanced.php
+++ advanced.php
@@ -15,15 +15,30 @@ echo "The time is " . date("h:i:sa");
 class Fruit {
 	public $name;
 	public $color;
-  
+
 	function __construct($name) {
-	  $this->name = $name;
+	$this->name = $name;
 	}
 	function __destruct() {
-	  echo "The fruit is {$this->name}.";
+	echo "The fruit is {$this->name}.";
+	}
+}
+
+$apple = new Fruit("Apple");
+
+class Fruit {
+	public $name;
+	public $color;
+  
+	function __construct($name, $color) {
+	  $this->name = $name;
+	  $this->color = $color;
+	}
+	function __destruct() {
+	  echo "The fruit is {$this->name} and the color is {$this->color}.";
 	}
   }
   
-  $apple = new Fruit("Apple");
+  $apple = new Fruit("Apple", "red");
 
 ?>
