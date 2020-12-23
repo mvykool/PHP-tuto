e955891ad5c4de06a133c03a9892897455f72dc8
maicol hernandez <maykolandres4@gmail.com>
2020-12-23 12:00:00 -0500

    commit of the day

--- advanced.php
+++ advanced.php
@@ -29,16 +29,16 @@ $apple = new Fruit("Apple");
 class Fruit2 {
 	public $name;
 	public $color;
-  
+
 	function __construct($name, $color) {
-	  $this->name = $name;
-	  $this->color = $color;
+	$this->name = $name;
+	$this->color = $color;
 	}
 	function __destruct() {
-	  echo "The fruit is {$this->name} and the color is {$this->color}.";
+	echo "The fruit is {$this->name} and the color is {$this->color}.";
 	}
-  }
-  
-  $apple = new Fruit("Apple", "red");
+}
+
+$apple = new Fruit("Apple", "red");
 
 ?>
