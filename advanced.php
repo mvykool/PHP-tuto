3643faa2fdd3a347e01db735116260aaf10692c6
maicol hernandez <maykolandres4@gmail.com>
2020-12-28 12:00:00 -0500

    commit of the day

--- advanced.php
+++ advanced.php
@@ -44,11 +44,11 @@ $apple = new Fruit("Apple", "red");
 trait message1 {
 	public function msg1() {
 		echo "OOP is fun! ";
-	  }
+	}
 	}
 	
 	class Welcome {
-	  use message1;
+	use message1;
 	}
 	
 	$obj = new Welcome();
