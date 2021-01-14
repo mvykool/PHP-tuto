072b0f1c8fdae0e77357b4dbff4d7b3e47d452aa
maicol hernandez <maykolandres4@gmail.com>
2021-01-14 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -90,14 +90,7 @@ if ($t < "20") {
 	}
 
 	$t = date("H");
-
-if ($t < "10") {
-	echo "Have a good morning!";
-} elseif ($t < "20") {
-	echo "Have a good day!";
-} else {
-	echo "Have a good night!  "; 
-}	
+	
 ?>
 
 </body>
