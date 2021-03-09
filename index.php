99f8fae16457e7ebb79cb33b5b89f6058655cadc
maicol hernandez <maykolandres4@gmail.com>
2021-03-09 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -72,6 +72,12 @@ echo $y;
 
 
 echo str_replace("world", "Dolly", "Hello world!sss"); // outputs Hello Dolly!
+
+$x = 5985;
+var_dump(is_int($x));
+
+$x = 59.85;
+var_dump(is_int($x));
 ?>
 
 </body>
