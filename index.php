d3f07bad6dadd0b9ede987edadc132f122bd0bdb
maicol hernandez <maykolandres4@gmail.com>
2021-05-14 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -26,6 +26,18 @@ myTest();
 
 echo "<p>Variable x outside function is: $x</p>";
 
+$x = 5;
+$y = 10;
+
+function myTest() {
+  global $x, $y;
+  $y = $x + $y;
+}
+
+myTest();
+echo $y; // outputs 15
+
+
 ?>
 
 </body>
