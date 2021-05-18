8acd5572161d0a432e4353cb4bbbc4f88e40aa24
maicol hernandez <maykolandres4@gmail.com>
2021-05-18 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -16,6 +16,16 @@ $txt = "Hello world!";
 $x = 5;
 $y = 10;
 
+$x = 5; // global scope
+
+function myTest() {
+  // using x inside this function will generate an error
+  echo "<p>Variable x inside function is: $x</p>";
+}
+myTest();
+
+echo "<p>Variable x outside function is: $x</p>";
+
 ?>
 
 </body>
