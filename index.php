0dd2b45feb9008851e80a6a94795c3c9e443a476
maicol hernandez <maykolandres4@gmail.com>
2021-06-03 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ echo $y; // outputs 15
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+echo "This ", "string ", "was ", "made ", "with multiple parameters. and more";
 ?>
 
 </body>
