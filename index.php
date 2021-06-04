2497aabb277fca9d86913e1d0e4622d707c4255d
maicol hernandez <maykolandres4@gmail.com>
2021-06-04 12:00:00 -0500

    commit of the day

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <body>
 
 <?php
-echo "Hello World!<br>";
-
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
 ?>
 
 </body>
